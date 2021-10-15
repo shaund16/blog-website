a51fba18afafb429a1a308cf17e950dd61126cb5
Shaun <s-purslow@hotmail.com>
2021-10-15 15:24:53 -0600

    make home component and styles for it

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../header/Header';
+import Posts from '../../posts/Posts';
+import Sidebar from '../../sidebar/Sidebar';
+import './Home.css';
+const Home = () => {
+  return (
+    <>
+      <Header/>
+    <div className='home'>
+    <Posts />
+    <Sidebar />
+    </div>
+    </>
+  )
+}
+
+export default Home
