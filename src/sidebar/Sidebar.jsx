058a660a0d52b6feee5fab358560311eb5f04f1c
Shaun <s-purslow@hotmail.com>
2021-10-18 20:09:14 -0600

    make the component and organize it

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -4,9 +4,40 @@ import './Sidebar.css';
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      sidebar
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>ABOUT ME</span>
+        <img
+          src='https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg'
+          alt=''
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro illum
+          ab sint praesentium, numquam iure facere voluptatibus saepe laboriosam
+          ex facilis aliquam ducimus dolorum, voluptatem iste suscipit? Unde,
+          accusamus ad?
+        </p>
+      </div>
+      <div className='sidebar'>
+        <span className='sidebarTitle'>CATEGORIES</span>
+        <ul className='sidebarList'>
+          <li className='sidebarListItem'>Life</li>
+          <li className='sidebarListItem'>Music</li>
+          <li className='sidebarListItem'>Style</li>
+          <li className='sidebarListItem'>Sport</li>
+          <li className='sidebarListItem'>Cinema</li>
+          <li className='sidebarListItem'>Tech</li>
+        </ul>
+      </div>
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>FOLLOW US</span>
+        <div className='sidebarSocial'>
+          <i className='sidebarIcon fab fa-facebook-square'></i>
+          <i className='sidebarIcon fab fa-twitter-square'></i>
+          <i className='sidebarIcon fab fa-github-square'></i>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
