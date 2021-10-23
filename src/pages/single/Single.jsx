6b85bcf84790b38dd69493a7cd78194334e496cb
Shaun <s-purslow@hotmail.com>
2021-10-22 23:08:37 -0600

    make a single page

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Single.css'
+import Sidebar from '../../components/sidebar/Sidebar'
+
+const Single = () => {
+  return (
+    <div className='single'>
+         {/*posts  */}
+         <Sidebar />
+    </div>
+  )
+}
+
+export default Single
