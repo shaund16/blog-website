709e91c86dbe843701258ca67a6892fa339001cf
Shaun <s-purslow@hotmail.com>
2021-10-22 23:04:57 -0600

    change to single page rendering on app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Home from './pages/home/Home';
+import Single from './pages/single/Single';
 import TopBar from './topbar/TopBar';
 function App() {
   return (
     <div className="App">
      <TopBar />
-     <Home />
+     <Single />
     </div>
   );
 }
