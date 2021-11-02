8accd7a0f7e33f44d7c5321a6bd1c5cf490e7caf
Shaun <s-purslow@hotmail.com>
2021-11-01 23:13:18 -0600

    add routes with conditions

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import Single from './pages/single/Single';
 import TopBar from './components/topbar/TopBar.jsx';
 import Write from './pages/write/Write';
 import Setting from './pages/settings/Setting';
-
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 function App() {
+  const user = false;
   return (
-    <div className="App">
-     <TopBar />
-     <Setting />
-    </div>
+    <Router>
+      <TopBar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/register'>{user ? <Home /> : <Register />}</Route>
+        <Route path='/login'>{user ? <Home /> : <Login />}</Route>
+        <Route path='/write'>
+          {user ? <Write /> : <Register />}
+        </Route>
+        <Route path='/settings'>
+          {user ? <Setting /> : <Register />}
+        </Route>
+        <Route path='/post/:postId'>
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
