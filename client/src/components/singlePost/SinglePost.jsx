bf847362ca6a7c834fe915f5099b6b4b9e6a3a6b
Shaun <s-purslow@hotmail.com>
2021-11-03 14:09:23 -0600

    get the posts from each blog

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,48 +1,45 @@
-import React from 'react'
-import './SinglePost.css'
+import React, { useState, useEffect } from 'react';
+import './SinglePost.css';
+import { useLocation } from 'react-router';
+import axios from 'axios';
 
 const SinglePost = () => {
+  const location = useLocation();
+  const path = location.pathname.split('/')[2];
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const getPost = async () => {
+      const response = await axios.get('/posts/' + path);
+      setPost(response.data);
+    };
+    getPost();
+  }, [path]);
   return (
     <div className='singlePost'>
-      <div className="singlePostWrapper">
-        <img 
-        src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1474&q=80" 
-        alt="" 
-        className="singlePostImg" 
-        />
-        <h1 className="singlePostTitle">Lorem, ipsum dolor sit amet.
-        <div className="singlePostEdit">
-        <i className="singlePostIcon far fa-edit"></i>
-        <i className="singlePostIcon far fa-trash-alt"></i>
-        </div>
+      <div className='singlePostWrapper'>
+        {post.photo && (
+          <img src={post.photo} alt='' className='singlePostImg' />
+        )}
+        <h1 className='singlePostTitle'>
+          {post.title}
+          <div className='singlePostEdit'>
+            <i className='singlePostIcon far fa-edit'></i>
+            <i className='singlePostIcon far fa-trash-alt'></i>
+          </div>
         </h1>
-        <div className="singlePostInfo">
-          <span className='singlePostAuthor'>Author: <strong>Shaun</strong></span>
-          <span className='singlePostDate'>1 hour ago</span>
+        <div className='singlePostInfo'>
+          <span className='singlePostAuthor'>
+            Author: <strong>{post.username}</strong>
+          </span>
+          <span className='singlePostDate'>
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-        <p className='singlePostDesc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt at tempore ipsum aliquam sapiente asperiores distinctio ipsa vel fugiat neque molestias necessitatibus ratione, vitae corporis quae repellendus officiis ex veniam.
-        Dicta doloribus asperiores quas eius provident laboriosam, magnam, nostrum maiores, tempora similique reprehenderit aspernatur quidem. Debitis numquam aspernatur, dolorem aliquid similique, odio distinctio accusamus et nihil ullam, harum fugiat earum?
-        Incidunt expedita similique, accusantium voluptatem sit, mollitia aliquam eligendi, earum recusandae a aperiam praesentium doloribus distinctio repellendus iure consequatur itaque dolorem deserunt exercitationem temporibus odit. Nisi architecto corrupti molestias nobis.
-        Suscipit quae possimus deleniti officia fugiat distinctio sequi, nulla sit minus ipsum porro incidunt dolores molestias cum iste. Consequatur, perspiciatis ipsum. Cum laborum repellendus, ipsam tenetur alias quam beatae excepturi.
-        Magni accusantium dignissimos dolor illo blanditiis illum consequatur, eos dicta aperiam accusamus minima a rem eveniet pariatur, quam id magnam doloribus et facilis, quas adipisci aliquam minus. Numquam, delectus saepe.
-        Eligendi, vel? Ex rem eos accusamus enim, veniam illo nulla quibusdam delectus blanditiis iure, sapiente animi maiores provident, ipsa odio tenetur recusandae totam ipsam ab ut. Ipsa esse quos pariatur.
-        Rerum cupiditate laboriosam quae veritatis dolores? Ipsam sequi quae, incidunt at officiis architecto tenetur totam! Ex, quia tenetur quibusdam sint maxime perspiciatis molestias repellat numquam? Repellat enim temporibus hic impedit.
-        Amet cumque ipsa minus voluptates possimus esse iure autem id distinctio explicabo. Eos sapiente accusantium tempore perferendis reiciendis minus quo, temporibus repellendus modi nihil. Expedita, iusto quaerat! Vel, iste laborum.
-        Itaque blanditiis pariatur dignissimos impedit laudantium amet illo architecto nesciunt, reprehenderit molestiae possimus nostrum eaque magnam non libero ratione veritatis, provident qui quo culpa quos in obcaecati nam? Sit, error!
-        Voluptatibus, rem quo. Harum totam placeat, voluptas fugit alias enim accusamus necessitatibus iure sapiente illum cupiditate, odit similique quas. Incidunt est sapiente excepturi quibusdam, enim id cum exercitationem voluptates repudiandae.
-        Neque id fugit expedita accusantium aliquam dolor fugiat, alias consequatur natus possimus ipsa suscipit quod quis magnam modi similique, ad architecto, quos itaque vitae repellendus. Illum labore excepturi natus vero.
-        Odit obcaecati, officia ab totam dicta magni blanditiis accusamus omnis minima exercitationem dignissimos ipsa? Delectus unde harum voluptas ad, blanditiis suscipit, ut dicta illo perspiciatis sequi recusandae magni fuga earum.
-        Vero asperiores sunt obcaecati libero mollitia necessitatibus, vitae consequatur repellendus culpa suscipit quas ad a sed. Doloremque placeat nisi, in veniam libero hic ab ipsum, necessitatibus ea, quia molestiae itaque!
-        Autem voluptas voluptatibus facilis odio at unde consequatur magnam nihil facere sunt architecto, quaerat obcaecati dolore dolorem, dolorum deserunt quasi impedit totam illum delectus rem quibusdam. Possimus rem maiores voluptas!
-        Expedita fuga sed error nam impedit voluptates inventore porro, tempore eaque aspernatur delectus saepe reprehenderit modi maxime ducimus deserunt similique laboriosam veritatis illo nisi ipsum qui beatae pariatur! Itaque, eum.
-        Placeat perspiciatis incidunt earum minus rerum nesciunt expedita fugit? Optio eaque deleniti esse blanditiis illum harum quibusdam? Ducimus omnis, facilis ex alias iusto, quam animi dolorum laudantium veritatis voluptatem molestiae!
-        Voluptatibus exercitationem est deleniti tenetur ea tempore quidem et velit molestiae porro? Quae esse cupiditate ullam tenetur ex totam voluptatibus, corrupti voluptates necessitatibus quibusdam impedit commodi nesciunt earum nam fuga.
-        Quibusdam exercitationem ab placeat quas nulla dicta veritatis voluptatibus itaque explicabo velit impedit, nobis accusantium corporis dolore provident perferendis quasi deserunt accusamus neque ipsum aliquid ea laudantium temporibus a? A.
-        Maxime nisi cupiditate sapiente quaerat, praesentium veritatis rem quasi corporis neque modi! Nobis tempore autem nemo quaerat architecto temporibus laboriosam perferendis itaque ratione, eos dicta quidem voluptas numquam delectus dolore.
-        Itaque, non facilis. Modi, ipsam, necessitatibus accusantium amet, totam numquam praesentium nesciunt fuga est iste soluta officia ex odio porro molestiae voluptates ipsa? Natus expedita quasi vero ab, sapiente ea?</p>
+        <p className='singlePostDesc'>{post.desc}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SinglePost
+export default SinglePost;
