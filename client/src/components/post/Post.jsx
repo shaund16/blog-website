b2f99526cd86f3547e6fb04d3d2fee6e4d7c1c0d
Shaun <s-purslow@hotmail.com>
2021-11-03 14:06:57 -0600

    add link from react router, pass props down to the post and map through the posts

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Post.css';
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
     <div className='post'>
-      <img className='postImg' src='https://images.unsplash.com/photo-1631539816626-9b5abd9acf9f?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDUyfDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60' alt='' />
+      {post.photo && <img className='postImg' src={post.photo} alt='' />}
       <div className='postInfo'>
         <div className='postCats'>
-          <span className='postCat'>Music</span>
-          <span className='postCat'>Life</span>
+          {post.categories.map((c) => (
+            <span className='postCat'>{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ut omnis accusamus expedita atque voluptate est voluptatem adipisci quam.</span>
-        <hr/>
-        <span className="postDate">1 hour ago</span>
+        <Link to={`/post/${post._id}`} className='link'>
+          <span className='postTitle'>{post.title}</span>
+        </Link>
+        <hr />
+        <span className='postDate'>
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className='postDesc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ullam eos voluptatum tempora? Adipisci voluptatum velit voluptate aspernatur rerum placeat aliquid, assumenda atque eveniet ipsa porro non consequatur tenetur quae.</p>
+      <p className='postDesc'>{post.desc}</p>
     </div>
   );
 };
