89aa44a6b3059614883c71ab528a008229b77761
Shaun <s-purslow@hotmail.com>
2021-11-03 14:10:12 -0600

    fetch the data from db and use state into posts component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/header/Header';
 import Posts from '../../components/posts/Posts';
 import Sidebar from '../../components/sidebar/Sidebar';
 import './Home.css';
+import axios from 'axios';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+useEffect(() => {
+  const fetchPosts = async () => {
+  const response = await axios.get('/posts')
+  setPosts(response.data)
+  }
+  fetchPosts()
+}, [])
+
   return (
     <>
       <Header/>
     <div className='home'>
-    <Posts />
+    <Posts posts={posts} />
     <Sidebar />
     </div>
     </>
